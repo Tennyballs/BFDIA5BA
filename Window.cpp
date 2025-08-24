2ce6dfaf46dc14e0e4f4a968fdd9f398d39b4b1a
Cuppy130 <player130games@gmail.com>
2025-08-24 00:07:11 -0700

    added accelerated graphics

--- Window.cpp
+++ Window.cpp
@@ -9,7 +9,7 @@ Window::Window(char *WindowTitle, int WindowWidth, int WindowHeight)
     this->WindowHeight = WindowHeight;
 
     window = SDL_CreateWindow(WindowTitle, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WindowWidth, WindowHeight, 0);
-    renderer = SDL_CreateRenderer(window, -1, 0);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
     SDL_SetRenderDrawColor(this->renderer, 0, 0, 0, 1);
     SDL_RenderClear(this->renderer);
